0b48a336c504374c8d54bdd56b9fe9d74eb2ef5b
Dipankar <dipankarmedhi91@gmail.com>
2023-02-07 10:59:16 -0500

    initial commit

---
+++ src/test/java/com/fhir/poc/appointment/AppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fhir.poc.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
