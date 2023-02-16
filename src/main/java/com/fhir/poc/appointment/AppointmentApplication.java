8da8dd5c41595aabb11ce688c3ce11aca15f8969
saurabh.gaur <saurabh.gaur@globallogic.com>
2023-02-16 11:43:19 -0600

    Adding Swaager

--- src/main/java/com/fhir/poc/appointment/AppointmentApplication.java
+++ src/main/java/com/fhir/poc/appointment/AppointmentApplication.java
@@ -1,15 +1,41 @@
 package com.fhir.poc.appointment;
 
+import com.fasterxml.classmate.TypeResolver;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class AppointmentApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppointmentApplication.class, args);
+	}
+	ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("Sprint Boot FHIR POC")
+				.license("Apache 2.0")
+				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+				.termsOfServiceUrl("")
+				.version("1.0.0")
+				.build();
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.fhir.poc"))
+				.build()
+				.apiInfo(apiInfo());
 	}
 
 //	@Bean
