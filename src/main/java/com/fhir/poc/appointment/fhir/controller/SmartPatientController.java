b5eb6d265cf932a8568ce04fef02bab9a58d688c
saurabh.gaur <saurabh.gaur@globallogic.com>
2023-02-23 13:21:47 -0600

    Adding Smart Controller

--- src/main/java/com/fhir/poc/appointment/fhir/controller/SmartPatientController.java
+++ src/main/java/com/fhir/poc/appointment/fhir/controller/SmartPatientController.java
@@ -1,8 +1,10 @@
 package com.fhir.poc.appointment.fhir.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import com.fhir.poc.appointment.fhir.models.Patient;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import java.net.URLEncoder;
@@ -10,17 +12,19 @@ import java.net.URLEncoder;
 @RestController
 public class SmartPatientController {
 
+    String baseUrl  = "https://launch.smarthealthit.org/v/r4/sim/WzQsIiIsIiIsIiIsMCwwLDAsIiIsIiIsIiIsIiIsIiIsIiIsIiIsMCwxXQ/fhir";
     @GetMapping("/smart/configuration")
     public ResponseEntity<?> getSmartConfiguration(){
-        String url = "https://launch.smarthealthit.org/v/r4/sim/WzQsIiIsIiIsIiIsMCwwLDAsIiIsIiIsIiIsIiIsIiIsIiIsIiIsMCwxXQ/fhir/.well-known/smart-configuration";
+        String url = baseUrl+"/.well-known/smart-configuration";
         RestTemplate restTemplate = new RestTemplate();
         Object configuration = restTemplate.getForObject(url,Object.class);
         System.out.println(configuration);
         return new ResponseEntity<Object>(configuration, HttpStatus.OK);
     }
+
     @GetMapping("/smart/patients")
     public ResponseEntity<?> getSmartPatient(){
-        String url = "https://launch.smarthealthit.org/v/r4/sim/WzQsIiIsIiIsIiIsMCwwLDAsIiIsIiIsIiIsIiIsIiIsIiIsIiIsMCwxXQ/fhir/Patient";
+        String url = baseUrl+"/Patient";
         RestTemplate restTemplate = new RestTemplate();
         Object patient = restTemplate.getForObject(url,Object.class);
         return new ResponseEntity<Object>(patient, HttpStatus.OK);
